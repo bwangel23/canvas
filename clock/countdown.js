5f89584ba86762030b38668844e8272cd0d69121
bwangel <bwangel.me@gmail.com>
2016-08-17 08:08:25 +0800

    feat(clock): add show balls

--- clock/countdown.js
+++ clock/countdown.js
@@ -3,13 +3,16 @@
  */
 
 var WINDOW_WIDTH = 1024;
-var WINDOW_HEIGHT = 768;
+var WINDOW_HEIGHT = 580;
 var Radius = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 
 var curShowTimeSeconds = 0;
 const endTime = new Date(2016, 7, 17, 18,47,52);
+
+var balls = [];
+const colors = ["#33B5E5", "#0099CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC00000"];
 
 window.onload = function () {
 
@@ -40,7 +43,58 @@ function update() {
     var curSecnods = curShowTimeSeconds % 60;
 
     if(nextSeconds != curSecnods) {
+        if(parseInt(curHours/10) != parseInt(nextHours/10)) {
+            addBalls(MARGIN_LEFT + 0, MARGIN_TOP, parseInt(curHours/10));
+        }
+        if(parseInt(curHours%10) != parseInt(nextHours%10)) {
+            addBalls(MARGIN_LEFT + 15 * (Radius + 1), MARGIN_TOP, parseInt(curHours%10));
+        }
+        if(parseInt(curMinutes/10) != parseInt(nextMinutes/10)) {
+            addBalls(MARGIN_LEFT + 39 * (Radius + 1), MARGIN_TOP, parseInt(curMinutes/10));
+        }
+        if(parseInt(curMinutes%10) != parseInt(nextMinutes%10)) {
+            addBalls(MARGIN_LEFT + 54 * (Radius + 1), MARGIN_TOP, parseInt(curMinutes%10));
+        }
+        if(parseInt(curSecnods/10) != parseInt(nextSeconds/10)) {
+            addBalls(MARGIN_LEFT + 78 * (Radius + 1), MARGIN_TOP, parseInt(curSecnods/10));
+        }
+        if(parseInt(curSecnods%10) != parseInt(nextSeconds%10)) {
+            addBalls(MARGIN_LEFT + 93 * (Radius + 1), MARGIN_TOP, parseInt(curSecnods%10));
+        }
         curShowTimeSeconds = nextShowTimeSeconds;
+    }
+    
+    update_balls();
+}
+
+function  addBalls(x, y, num) {
+    for(var i = 0; i < digit[num].length; i++) {
+        for (var j = 0; j < digit[num][i].length; j++) {
+            if(digit[num][i][j] == 1) {
+                var Ball = {
+                    x: x + j * 2 * (Radius + 1) + (Radius + 1),
+                    y: y + i * 2 * (Radius + 1) + (Radius + 1),
+                    g: 1.5 + Math.random(),
+                    vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,
+                    vy: -5,
+                    color: colors[Math.floor(Math.random()*colors.length)]
+                };
+                balls.push(Ball);
+            }
+        }
+    }
+}
+
+function update_balls() {
+    for(var i = 0; i < balls.length; i++) {
+        balls[i].x += balls[i].vx;
+        balls[i].y += balls[i].vy;
+        balls[i].vy += balls[i].g;
+
+        if(balls[i].y >= WINDOW_HEIGHT - Radius) {
+            balls[i].y = WINDOW_HEIGHT - Radius;
+            balls[i].vy = -balls[i].vy * 0.75;
+        }
     }
 }
 
@@ -70,6 +124,16 @@ function render(context) {
     renderDigit(MARGIN_LEFT + 69 * (Radius + 1), MARGIN_TOP, parseInt(10), context);
     renderDigit(MARGIN_LEFT + 78 * (Radius + 1), MARGIN_TOP, parseInt(seconde/10), context);
     renderDigit(MARGIN_LEFT + 93 * (Radius + 1), MARGIN_TOP, parseInt(seconde%10), context);
+
+    for(var i = 0; i < balls.length; i++) {
+        context.fillStyle = balls[i].color;
+
+        context.beginPath();
+        context.arc(balls[i].x, balls[i].y, Radius, 0, 2*Math.PI, true);
+        context.closePath();
+
+        context.fill()
+    }
 }
 
 function renderDigit(x, y, num, context) {
