495a8cae8587936ff30e9b016651602d769b4d4c
bwangel <bwangel.me@gmail.com>
2016-09-02 07:43:40 +0800

    feat(font): get music data

---
+++ show-music/public/javascripts/index.js
@@ -0,0 +1,30 @@
+/**
+ * Created by yundongx on 9/2/16.
+ */
+
+function $(s) {
+    return document.querySelectorAll(s);
+}
+
+var list = $("#list li");
+
+for(var i = 0; i < list.length; i++) {
+    list[i].onclick = function () {
+        for(var j = 0; j < list.length; j++) {
+            list[j].className = "";
+        }
+        this.className = "selected";
+        load("/media/" + this.title);
+    }
+}
+
+var xhr = new XMLHttpRequest();
+
+function load(url) {
+    xhr.open("GET", url);
+    xhr.responseType = "arraybuffer";
+    xhr.onload = function () {
+        console.log(xhr.response);
+    }
+    xhr.send();
+}
