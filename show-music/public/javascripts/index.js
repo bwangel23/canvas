eabc38afa44d3c4c3b62a9d4b775c07e02c68a5c
bwangel <bwangel.me@gmail.com>
2016-09-13 07:55:15 +0800

    feat(show): add showing dot

--- show-music/public/javascripts/index.js
+++ show-music/public/javascripts/index.js
@@ -37,30 +37,35 @@ var height, width;
 var canvas = document.createElement("canvas");
 var context = canvas.getContext("2d");
 box.appendChild(canvas);
+var Dots = [];
+function random(m, n) {
+    return Math.round(Math.random() * (n - m) + m);
+}
+
+function getDots() {
+    Dots = []
+    for(var i = 0;i < size;i++) {
+        var x = random(0, width);
+        var y = random(0, height);
+        var color = "rgb(" + random(0, 255) + "," + random(0,255) + "," 
+            + random(0, 255) + ")";
+        Dots.push({
+            x: x,
+            y: y,
+            color: color
+        })
+    }
+}
 
 function resize() {
     height = box.clientHeight;
     width = box.clientWidth;
     canvas.height = height;
     canvas.width = width;
-    var line = context.createLinearGradient(0, 0, 0, height);
-    line.addColorStop(0, "red");
-    line.addColorStop(0.5, "yellow");
-    line.addColorStop(1, "green");
-    context.fillStyle = line;
+    getDots();
 }
 resize();
 window.onresize = resize;
-
-var types = $("#type li");
-for(var i = 0; i < types.length; i++) {
-    types[i].onclick = function () {
-        for(var j = 0; j < types.length; j++) {
-            types[j].className = "";
-        }
-        this.className = "selected";
-    }
-}
 
 function load(url) {
     var n = ++counter;
@@ -88,8 +93,42 @@ function draw(arr) {
     context.clearRect(0, 0, width, height);
     var w = width / size;
     for(var i = 0; i < size; i++){
-        var h = arr[i] / 256 * height;
-        context.fillRect(w * i, height - h, w * 0.6, h);
+        if(draw.type == "column") {
+            var h = arr[i] / 256 * height;
+            var line = context.createLinearGradient(0, 0, 0, height);
+            line.addColorStop(0, "red");
+            line.addColorStop(0.5, "yellow");
+            line.addColorStop(1, "green");
+            context.fillStyle = line;
+            context.fillRect(w * i, height - h, w * 0.6, h);
+        } else if(draw.type == "dot") {
+            context.beginPath();
+            var o = Dots[i];
+            var r = arr[i] / 256 * 50;
+            context.arc(o.x, o.y, r, 0, Math.PI * 2, true);
+            var g = context.createRadialGradient(o.x, o.y, 0, o.x, o.y, r);
+            g.addColorStop(0, "#fff");
+            g.addColorStop(1, o.color);
+            context.fillStyle = g;
+            context.fill();
+            // context.stroke();
+            context.closePath();
+        }
+    }
+}
+
+draw.type = "column";
+
+var types = $("#type li");
+for(var i = 0; i < types.length; i++) {
+    types[i].onclick = function () {
+        for(var j = 0; j < types.length; j++) {
+            types[j].className = "";
+        }
+        this.className = "selected";
+        draw.type = this.getAttribute("data-type");
+        console.log("draw.type", draw.type);
+        console.log(draw.type);
     }
 }
 
